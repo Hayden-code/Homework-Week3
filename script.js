393fdd680f084a29acb1c2284ba30885d438131f
Hayden Jones <haychjones@gmail.com>
2021-03-16 22:23:42 +1100

    password generator function added

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var SC = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","^","_","`","{","|","}","~"];
+var characterNum = document.getElementById("character")
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 function getSelectedCheckboxValues(name) {
   const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
@@ -22,11 +18,54 @@ function getSelectedCheckboxValues(name) {
   return values;
 }
 
+function generatePassword() {
+  var values = getSelectedCheckboxValues('checkbox')
+  var password = ''
+  var pwordLength = document.querySelector("#characters").value;
+  if (values.includes("Lowercase")){
+    if (values.length = 3){
+      values.length = 2
+    }
+    function returnCharacter(){
+      var abcIndex = Math.floor(Math.random() * abc.length);
+      var abcChosen = abc[abcIndex];
+      return abcChosen;
+    }
+    for (var i = 0; i < pwordLength / values.length; i++) {
+      password += returnCharacter();
+    }
+  }
+  if (values.includes("Uppercase")){
+    function returnCharacter(){
+      var abcIndex = Math.floor(Math.random() * abc.length);
+      var abcChosen = abc[abcIndex];
+      return abcChosen;
+    }
+    for (var i = 0; i < pwordLength / values.length; i++) {
+      password += returnCharacter().toUpperCase();
+    }
+  }
+  if (values.includes("Number")){
+    function returnNumber(){
+      var randomNum = Math.floor(Math.random() * 10);
+      return randomNum;
+    }
+    for (var i = 0; i < pwordLength / values.length; i++) {
+      password += returnNumber();
+    }
+  }
+  if (values.includes("specialCharacter")){
+    function returnSC(){
+      var SCIndex = Math.floor(Math.random() * SC.length);
+      var SCChosen = SC[SCIndex];
+      return SCChosen;
+    }
+    for (var i = 0; i < pwordLength / values.length; i++) {
+      password += returnSC();
+    }
+  }
 
+  return password;
+}
 
-
-
-generateBtn.addEventListener('click', (event) => {
-  console.log(getSelectedCheckboxValues('checkbox'));
-});
-
+generateBtn.addEventListener("click", writePassword);
